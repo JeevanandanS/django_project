df4f713980273821d807be9ecd57f45475650fc3
jeevanandan1234 <tojeeva14@gmail.com>
2024-04-08 11:43:27 +0530

    new changes

--- project3/password_generator_post/generator/views.py
+++ project3/password_generator_post/generator/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# generator/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+    return render(request, "generator\index.html")
+
+def password(request):
+    if request.method == 'POST':
+        characters = list("abcdefghijklmnopqurstuvwxyz")
+
+        length = int(request.POST.get("length"))
+
+        if request.POST.get("uppercase"):
+            characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+        if request.POST.get("numbers"):
+            characters.extend(list("1234567890"))
+
+        if request.POST.get("special_characters"):
+            characters.extend(list("!@#$%^&*()-_+={}[]|\\:;\"'"))
+
+        password = ""
+        for x in range(length):
+            password = password + random.choice(characters)
+        return render(request, "generator\index.html", {"password": password})
+    return HttpResponse("Wrong Request")
