18d496194b21dbf35cc6e40a255a33b188d33153
jeevanandan1234 <tojeeva14@gmail.com>
2024-04-10 13:30:28 +0530

    newly made

--- my_project2/model_project/bustimings/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BustimingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bustimings'
--- my_project2/model_project/bustimings/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('bus-timings/', views.bus_timings, name='bus_timings'),
-]
